c8e814dca58c8a92e830de17ee61d65c67932047
Glitch (gigantic-danger) <none>
2018-12-31 16:09:18 +0000

    ⛳️💺 APK Fetcher

---
+++ translation.py
@@ -0,0 +1,8 @@
+class Translation(object):
+    START_TEXT = "Welcome to our bot. I am capable of downloading your best apps within seconds. \n\nI can do some basic functions for now which you can discover by navigating by yourself. Hope you find me useful 😊\n\nFor more help use the help button or just send /help\n\n"
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "No upgrades!"
+    NOT_AUTH_USER_TEXT = "The bot developer is a selfish dude. for now you can\'t download anything for this is just the beta bot. try the main bot at @gmotherbot"
+    CUSTOM_CAPTION_UL_FILE = " © @Bfas237Bots"
+    HELP_TEXT = "Below you can find the basic usage for your needs\n\nwith **Apk fetcher**, you don't need any apk file url but just the name which should be very straight forward and easy to do. It goes like this:\n\n\n🧧 Enter file name as part or full name\n\n🧧 Choose from the list of available results\n\n🧧 Wait for a while as it downloads and sends to you\n\n🧧 Enjoy your downloaded app 😎\n\n`⏳ More Features and updates will be available soon` "
+    DOWNLOAD_TOOLS_TEXT = "🆑 What is the name of that android app that you have been longing to download?\nThis bot will awesomely get it for you in no time.\n\nFor now no file size limit has been implemented so enjoy 😎."
