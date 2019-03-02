c19143159b458700b48386a191bedfdfc7121827
Ningmua Bruno <gwatanajohn3@gmail.com>
2019-03-02 15:50:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -597,65 +597,60 @@ def pyrogram_data(bot, update):
     )    
 def command_get_specify_apk(bot, update):
     if active_chats.get(update.from_user.id).get('link') is None:
-      search_query = active_chats.get(update.from_user.id).get('search_query')
+        search_query = active_chats.get(update.from_user.id).get('search_query')
     query = " ".join(search_query) 
-    sent = bot.send_message(update.from_user.id, 
-        text=fetching_download_link.format(query),
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True)
-    print('Searching for: {}'.format(query))
-    options={}
-    base_headers = {
+    try:
+        sent = update.reply(fetching_download_link.format(query),
+        quote=True, disable_web_page_preview=True)
+        print('Searching for: {}'.format(query))
+        options={}
+        base_headers = {
         'User-Agent':  'Mozilla/6.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.5 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.5',
         'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'zh-CN,zh;q=0.8'
     }
-    headers = dict(base_headers, **options)
-    res = requests.get('https://apkpure.com/search?q={}&region='.format(quote_plus(query)), headers=headers).text
-    APPS = []
-    soup = BeautifulSoup(res, "html.parser")
-    for i in soup.find('div', {'id':'search-res'}).findAll('dl', {'class':'search-dl'}):
-        app = i.find('p', {'class':'search-title'}).find('a')
-        APPS.append((app.text,
+        headers = dict(base_headers, **options)
+        res = requests.get('https://apkpure.com/search?q={}&region='.format(quote_plus(query)), headers=headers).text
+        APPS = []
+        soup = BeautifulSoup(res, "html.parser")
+        for i in soup.find('div', {'id':'search-res'}).findAll('dl', {'class':'search-dl'}):
+            app = i.find('p', {'class':'search-title'}).find('a')
+            APPS.append((app.text,
                     i.findAll('p')[1].find('a').text,
                     'https://apkpure.com' + app['href']))
-    time.sleep(5)
-    if len(APPS) == 0:
-      bot.edit_message_text(text="**📱 Apk Downloader Premium**\n\n__Step 2 of  2__\n"
-                              "\n\n🔍 Search for **{}** Returned (`0`) results\n\n You may try again by entering an altenative search and i will find it for you".format(search_query),
-                         chat_id=update.from_user.id,
-                         parse_mode="Markdown",
-                         message_id=sent.chat_id,
-                         #reply_markup=reply_markup,
-                         disable_web_page_preview=True)
-      return
-      
+        time.sleep(2)
     
-    inline_keyboard = []
-    if len(APPS) > 0:
-      for idx, app in enumerate(APPS):
+    
+        inline_keyboard = []
+    
+        if len(APPS) > 0:
+            sent.delete()
+            items = ""
+            for idx, app in enumerate(APPS):
+                items +=  ("[{:02d}]  -  {}".format(idx, app[0]))
         
-        start_string = "{}|{}".format(idx, app[0])
-        ikeyboard = [
-                            InlineKeyboardButton(
-                                "[{:02d}]  -  {}".format(idx, app[0]),
-                                callback_data=start_string.encode("UTF-8")
-                            )
-                        ]
-        user_chat = active_chats.get(update.from_user.id, None)
-        user_chat['Aps'] = APPS
-        user_chat['Apps'] = None      
-        inline_keyboard.append(ikeyboard)
-        num=len(APPS)
-        reply_markup = InlineKeyboardMarkup(inline_keyboard)
-        bot.edit_message_text(
-        text="**📱 Apk Downloader Premium**\n\n__Step 2 of  2__\n"
-                              "\n\n🔍 Search for **{}** Returned (`{}`) results\n\n Click on your app and i will download it right away".format(search_query, num),
-        chat_id=update.from_user.id,
+                start_string = "{}|{}".format(idx, app[0])
+            ikeyboard = [InlineKeyboardButton(items, callback_data=start_string.encode("UTF-8"))]
+            inline_keyboard.append(ikeyboard)
+            user_chat = active_chats.get(update.from_user.id, None)
+            user_chat['Aps'] = APPS
+            user_chat['Apps'] = None      
         
-        reply_markup=reply_markup,
-        message_id=sent.message_id,
-        disable_web_page_preview=True)
+            num=len(APPS)
+            reply_markup = InlineKeyboardMarkup(inline_keyboard)
+            bot.edit_message_text(text="**📱 Apk Downloader Premium**\n\n__Step 2 of  2__\n"
+                              "\n\n🔍 Search for **{}** Returned (`{}`) results\n\n Click on your app and i will download it right away".format(search_query, num), chat_id=update.from_user.id, reply_markup=reply_markup, message_id=sent.message_id, disable_web_page_preview=True)
+        else:
+            sent.edit("**📱 Apk Downloader Premium**\n\n__Step 1 of  2__\n"
+                              "\n\n❗️ Search for **{}** Returned (`0`) results\n\n You may try again by entering an altenative search and i will find it for you".format(search_query))
+            return
+
+    except Exeception as e:
+        update.reply(str(e))
+    except:
+      traceback.print_exc()
+
+
         
 @app.on_callback_query(dynamic_data(b"help"))
 def pyrogram_data(bot, update):
